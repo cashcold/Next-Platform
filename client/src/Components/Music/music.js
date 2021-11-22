acbea371f6f7c1997e8db5c48943524fc25c6aa4
cashcold <cashcold99@gmail.com>
2021-11-22 08:33:06 +0000

    final solution for gsap parallax pin div on Music component

--- client/src/Components/Music/music.js
+++ client/src/Components/Music/music.js
@@ -13,12 +13,28 @@ class MusicMain extends Component {
     }
 
     componentDidMount(){
-        // gsap.registerPlugin(ScrollTrigger)
+        gsap.registerPlugin(ScrollTrigger)
        
 
           
         const RegisterMusicMainTrigger = ()=>{
+            const T_L = gsap.timeline()
             
+            T_L
+            .from('.music_prara_box_1',{xPercent: -100})
+            .from('.music_prara_box_2',{xPercent: 100})
+            .from('.music_prara_box_3',{xPercent: -100})
+            .from('.music_prara_box_4',{xPercent: -100})
+
+            ScrollTrigger.create({
+                animation: T_L,
+                trigger: '.music_para',
+                start: '55px 0%',
+                end: '+=1000',
+                scrub: true,
+                pin: true,
+                anticipatePin: 1
+            })
         }
           
         RegisterMusicMainTrigger()
@@ -35,6 +51,12 @@ class MusicMain extends Component {
                     <link rel="canonical" href="next-platform.com" />
                 </Helmet>
                 <h1>MUSIC</h1>
+                <div className="music_para">
+                    <section className="music_prara_box_1"></section>
+                    <section className="music_prara_box_2"></section>
+                    <section className="music_prara_box_3"></section>
+                    <section className="music_prara_box_4"></section>
+                </div>
             </div>
          );
     }
