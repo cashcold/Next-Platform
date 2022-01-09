eb973193e932685bbd5767a6c11bc59cc26246cf
cashcold <cashcold99@gmail.com>
2022-01-09 16:53:10 +0000

    update sport

--- client/src/Components/SportMain/sportMain.js
+++ client/src/Components/SportMain/sportMain.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './sportMain.css'
 import axios from 'axios'
+import moment from 'moment'
 
 import {Card,Button} from 'react-bootstrap'
 class SportMain extends Component {
@@ -11,10 +12,11 @@ class SportMain extends Component {
          }
     }
     componentDidMount(){
-        axios.get(`https://newsapi.org/v2/everything?q=sports&apiKey=2e98525723394a6bbe973d45e9af7afa`)
+        axios.get(`http://site.api.espn.com/apis/site/v2/sports/football/college-football/news`)
         .then((data)=>{
             this.setState({
                 news_api_main: data.data.articles
+                // news_api_main: data.data.articles
             })
         })
     }
@@ -25,18 +27,21 @@ class SportMain extends Component {
                 <section className="sport_main_section_1"></section>
                 <section className="sport_main_section_2">
                     <div className="otherTopLine">
-                        <h1>Other Top Lines</h1>
+                        <h1>NCAAF News</h1>
                     </div>
                     <div className="infor_other_top">
                     {this.state.news_api_main.map(data => {
                             return(
                                 <div>
                                      <Card classMame='card_sport' style={{backgroundColor: "red", color: 'white', margin: '2em 0em'}}>
-                                        <Card.Img src={data.urlToImage} />
+                                        <Card.Img src={data.images[0].url} />
                                         <Card.Body>
-                                            {/* <Card.Title>Must Read</Card.Title> */}
                                             <Card.Text>
-                                            <h3>{data.title}</h3>
+                                            {/* <h3>{data.title}</h3> */}
+                                            <h2>{data.images[0].name}</h2>
+                                            <h3>{data.images[0].caption}</h3>
+                                            {/* <h5>Post: <span>{data.published}</span></h5> */}
+                                            <h5>Post: <span>{moment(data.published).format('LLLL')}</span></h5>
                                             </Card.Text>
                                             <a href='#' className='btn btn-warning'>Find More <i class="fas fa-arrow-circle-right"></i></a>
                                         </Card.Body>
