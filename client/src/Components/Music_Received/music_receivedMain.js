16c13ae1ad10529fd40d88abd72f5d125347bfe0
cashcold <cashcold99@gmail.com>
2022-02-05 15:15:04 +0000

    update json

--- client/src/Components/Music_Received/music_receivedMain.js
+++ client/src/Components/Music_Received/music_receivedMain.js
@@ -11,8 +11,17 @@ class MusicReceivedMain extends Component {
                 <section className="receivedMusic_section_1 music_LeftSideBar"></section>
                 <section className="receivedMusic_section_1 music_center_main">
                     <div className="iframe_list">
-                        <iframe  src="https://open.spotify.com/embed/playlist/3M75W37HkFPdnkkaaOhvPo?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
+                        <h2>Top Naja Count Down</h2>
+                        <iframe  src="https://open.spotify.com/embed/playlist/3M75W37HkFPdnkkaaOhvPo?utm_source=generator&theme=0" width="100%" height="400" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
                         </iframe>
+                    </div>             
+                    <div className="iframe_list">
+                        <h2>Top Gh Count Down</h2>
+                        <iframe  src="https://open.spotify.com/embed/playlist/3M75W37HkFPdnkkaaOhvPo?utm_source=generator&theme=0" width="100%" height="400" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                    </div>             
+                    <div className="iframe_list">
+                        <h2>Top 100 GHANA</h2>
+                        <iframe src="https://open.spotify.com/embed/playlist/4AJYrpCpoZdvwCC1YGmrct?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
                     </div>             
                </section>
                 <section className="receivedMusic_section_1 music_RightSideBar"></section>
