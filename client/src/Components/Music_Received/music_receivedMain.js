605cf038fffb5fa2e62891b736e9cdb961e945b2
cashcold <cashcold99@gmail.com>
2022-02-16 15:23:03 +0000

    working on music tree

--- client/src/Components/Music_Received/music_receivedMain.js
+++ client/src/Components/Music_Received/music_receivedMain.js
@@ -78,7 +78,7 @@ class MusicReceivedMain extends Component {
                     </div>
                     <div className="gosple_main">
                         <h2>Nigerian Mix Go</h2>
-                        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/5v0nHekCRbODpHtDgPnAwX?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                        <iframe  src="https://open.spotify.com/embed/playlist/5v0nHekCRbODpHtDgPnAwX?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
                     </div>
                 </section>
             </div>
