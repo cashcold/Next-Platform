bcb0e407d871cfe8deaa27d6c427706c484eebff
cashcold <cashcold99@gmail.com>
2022-02-26 12:32:45 +0000

    update on linkBox

--- client/src/Components/Link_Box/link_box.js
+++ client/src/Components/Link_Box/link_box.js
@@ -25,7 +25,7 @@ class LinkBoxMain extends Component {
                 </Helmet>
                 <section className="next_watch_section_1">
                     <a target='_blank' href='https://www.facebook.com/yellowcardapp'>
-                    <img src='https://scontent.facc8-2.fna.fbcdn.net/v/t39.30808-6/273451231_5677950762231699_7917947454472796315_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=2c4854&_nc_eui2=AeFTECg4-loQ_oJHfcIuSPPxKGfIWEGT3ckoZ8hYQZPdyRYlsH0fmWZhNTRz__IjmzNStJeRsYfZNVakf9kf-j96&_nc_ohc=vi9ok8fCaswAX-lY_Di&_nc_ht=scontent.facc8-2.fna&oh=00_AT-Wxj-IqSZJHL-Ps7ICvNACC7HsjeXQZdv-VOzk9kCoCg&oe=62195162'/>
+                    <img src={require('../../AllInOne/img2/273451231_5677950762231699_7917947454472796315_n.jpg')}/>
                     <h3> ®️Sign up today to start enjoying the best rates when 🤑 buying and selling digital assets on Yellow Card! 👍
                     Use promo Code 🎁YCGH to get 🥰15GHC.  <br/> Don't miss out!!!👉</h3>
                     <h4 className='btn btn-warning'>Find More</h4>
@@ -33,7 +33,7 @@ class LinkBoxMain extends Component {
                 </section>  
                 <section className="next_watch_section_1">
                     <a target='_blank' href='https://signup.bayportghana.com/payroll_loan/?utm_source=facebook&utm_medium=cpm&utm_campaign=L4_VP_PAYROLL_MAN%20DESK%20NEW_14.02.2022&utm_content=MAN%20DESK%20NEW&utm_term=23849943086460210%20Facebook_Desktop_Feed&fbclid=IwAR2CTgJwpiFBpCxDnPKpIiU2RMwup0qfl6hIqczAUtJ46srNEmRV0ymEFOI'>
-                    <img src='https://scontent.facc8-2.fna.fbcdn.net/v/t45.1600-4/cp0/q90/spS444/p526x296/273851525_23849943095780210_177150845390722455_n.png.jpg?_nc_cat=106&ccb=1-5&_nc_sid=e84a38&_nc_eui2=AeEOu-TRMDvUj16VQk8Sy3WD3u7O_ueFQIne7s7-54VAiUQM3vAv1ROt9e4fefl98NBMUXkfnsBgtg3N6IgELrMc&_nc_ohc=MewJYLlz500AX9FWRsj&_nc_ht=scontent.facc8-2.fna&oh=00_AT9lztIql_wC9O0scds84SJ6_pWKvKjkLjIBFCmZGcr-1Q&oe=62187DAB'/>
+                    <img src={require('../../AllInOne/img2/273851525_23849943095780210_177150845390722455_n.jpg')}/>
                     <h3> Get a loan up to Ghc 🤑200,000 with just a call!📱
                     At Bayport we have a financial solution for you🤟.
                     For CAGD government workers only⚠️.  <br/> Don't miss out!!!👉</h3>
@@ -49,14 +49,14 @@ class LinkBoxMain extends Component {
                 </section>  
                 <section className="next_watch_section_1">
                     <a target='_blank' href='https://www.mpowerfinancing.com/'>
-                    <img src='https://scontent.facc8-1.fna.fbcdn.net/v/t39.30808-6/273715674_3227024697583742_5980608295611256206_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeHPgUTO0wDIhfvYBzYxY4U5FQjXgYxLQPwVCNeBjEtA_H4S_WWCAAvuBPbVgiI4m1f7NAUkgzs50BBtLVUPck_x&_nc_ohc=d6D-KkkNCe0AX-4GYAP&_nc_ht=scontent.facc8-1.fna&oh=00_AT8OfKri-NAfU0V9LmmAhgTOotZ2uS2jSJgkf0jdesmjWQ&oe=62191F1C'/>
+                    <img src={require('../../AllInOne/img2/49270406_2317460395206848_5994933837296041984_n.png')}/>
                     <h3> Our 🧑‍🤝‍🧑student loans help you achieve your education goals. MPOWER’s loans have fixed interest rates with flexible loan amounts up to US$100,000 that can be used to cover tuition, school🏫 supplies, and living✔️ expenses for future semesters or past due balances. 👫We also support all majors and degrees with no requirement for a cosigner, collateral, or existing credit history.<br/> Don't miss out!!! 👉 </h3>
                     <h4 className='btn btn-warning'>Find More</h4>
                     </a>
                 </section>
                 <section className="next_watch_section_1">
                     <a target='_blank' href='https://cardvest.ng/'>
-                    <img src='https://scontent.facc8-2.fna.fbcdn.net/v/t45.1600-4/cp0/q75/spS444/p526x296/273632989_23850089667700397_5903033884409116475_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=e84a38&_nc_eui2=AeH_DGXiedotzw4hHFrrmX1rSc5pp8BC9-9JzmmnwEL377XAp-Nfwp_eu6pm4ftL3ffjWWXLEeY-aRam6kvlpdu5&_nc_ohc=YG5sQpz7ex0AX8cnrZ5&_nc_ht=scontent.facc8-2.fna&oh=00_AT-UgOMD9tGvbATef2bnisL3sxG3z1ZuPyf9hxdvf5b2zg&oe=6218FF22'/>
+                    <img src={require('../../AllInOne/img2/274034570_499353405080470_3857785958542022341_n.jpg')}/>
                     <h3> SELL 🎁GIFT CARDS📇 at best rates<br/>
                      Most profitable and customer-focused gift card trading platform in Nigeria and Ghana.
                     Stop trading your giftcard at a loss!<br/> Don't miss out!!! 👉 </h3>
