c20cf5c89da80ac6cc456b2b0bb4983138dada94
cashcold <cashcold99@gmail.com>
2022-02-26 11:06:00 +0000

    new update

--- client/src/Components/Watch_Next/watch_next.js
+++ client/src/Components/Watch_Next/watch_next.js
@@ -26,8 +26,8 @@ class WatchNextMain extends Component {
         return ( 
             <div className='watch_next_main'>
                 <section className="next_watch_section_1">
-                    <a target='_blank' href='https://www.facebook.com/johnben.annan.1'>
-                    <img src='https://scontent.facc8-2.fna.fbcdn.net/v/t39.30808-6/s720x720/274582099_487492182979817_2869515671241834304_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeGjoALWm1TewNdi4tQPeqd_SOLbFduYCrRI4tsV25gKtGfV6459WDyzExPHh_QRHMjncW_1OxgT9kQhAjTQCzI5&_nc_ohc=qMLi-HBnugQAX93Q4zi&tn=CrG_bAVXfnDePAKQ&_nc_ht=scontent.facc8-2.fna&oh=00_AT8Vw1gZsiMLcZXR5DRqgaSY3EXzlPLcY1aonPIHLOV8HA&oe=6219CB41'/>
+                    <a target='_blank' href='https://www.youtube.com/channel/UCsepKOc2Z2iwD8sjIt0h2jA'>
+                    <img src='https://scontent.facc8-2.fna.fbcdn.net/v/t1.6435-9/109750655_140723594323346_994710098553268757_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=e3f864&_nc_ohc=3URwMJ-Q7lgAX_bMNGs&_nc_ht=scontent.facc8-2.fna&oh=00_AT91_NPZTaeRg8XoQyMqf198I4rrhDjKBR43yv9uf2XEDg&oe=623EDEC8'/>
                     <h3> 🙏Worshper J B Annan<br/> Don't miss out!!! 👉 </h3>
                     <h4 className='btn btn-warning'>Find More</h4>
                     </a>
