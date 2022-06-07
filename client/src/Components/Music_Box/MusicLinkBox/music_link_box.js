64c60ceb802d1ea8f1846d1eeb33d1e64999eb63
cashcold <cashcold99@gmail.com>
2022-06-07 06:12:09 +0000

    working on social icon

--- client/src/Components/Music_Box/MusicLinkBox/music_link_box.js
+++ client/src/Components/Music_Box/MusicLinkBox/music_link_box.js
@@ -74,12 +74,11 @@ class MusicLinkBox extends Component {
             <Helmet>
                 <base />
                 <title> {this.state.mp3_api_head_Text}</title>
-                <meta name={this.state.mp3_api_about_Main} content="React hel/ment is useful for seo for dynamically changing head information" />
+                <meta name="desscription" content={this.state.mp3_api_about_Main} />
                 <meta property="og:title" content={this.state.mp3_api_head_Text} />
-                <meta property="og:description" content={this.state.mp3_api_about_Main} />
                 <meta property="og:url" content={this.state.home_url_location} />
                 <meta property="og:image" content="https://nest-platform.herokuapp.com/static/media/Amb-Lawrence-I-Still-Believe.0ad92c107bc5a518840e.jpg" />
-                <link rel="canonical" href="somelink" />
+                <link rel="canonical" href={this.state.home_url} />
             </Helmet>
                 <section className="link_view_1">
                     <div className='both_view'>
