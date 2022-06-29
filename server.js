232dbcba5cec8b36f2a56abbe8ae1763e5d8c538
cashcold <cashcold99@gmail.com>
2022-06-29 04:52:23 +0000

    new

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ const path = require('path')
 var SpotifyWebApi = require('spotify-web-api-node');
 const fs = require('fs');
 var Ebay = require('ebay-node-api')
+const axios = require('axios')
+const queryString = require('query-string');
+const { parse } = require('querystring')
 
 dotEnv.config()
+
+
 
 
 mongoose.connect(process.env.DataBaseConnecting,{ useNewUrlParser: true,  useUnifiedTopology: true },()=>{
@@ -20,15 +25,17 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 
+const Router = express.Router()
 app.use(cors())
 app.use(bodyParser.json())
-
 app.use('/users',userRouter)
+app.use(Router)
+
+
 
 
 
 app.get('/', function(request, response) {
-    console.log('Home page visited! okay');
     const filePath = path.resolve(__dirname, './client/build' ,'index.html');
   
     // read in the index.html file
@@ -44,8 +51,16 @@ app.get('/', function(request, response) {
       response.send(result);
     });
   });
-app.get('/music/gosple/id', function(request, response) {
-    console.log('Home page visited! okay');
+
+
+  Router.post('/meta_tags',(req,res)=>{
+    // console.log(req.body)
+      res.send(req.body)
+  })
+  
+
+app.get('/music', function(request, response) {
+ 
     const filePath = path.resolve(__dirname, './client/build' ,'index.html');
   
     // read in the index.html file
@@ -62,9 +77,8 @@ app.get('/music/gosple/id', function(request, response) {
     });
   });
 
-
-app.get('/link_box', function(request, response) {
-    console.log('Home page visited! okay');
+app.get('/music/:id', function(request, response) {
+ 
     const filePath = path.resolve(__dirname, './client/build' ,'index.html');
   
     // read in the index.html file
@@ -73,15 +87,58 @@ app.get('/link_box', function(request, response) {
         return console.log(err);
       }
       
-      // replace the special strings with server generated strings
-      data = data.replace(/\$OG_TITLE/g, 'Link Box Music NEXT-PLATFORM-HOME');
-      data = data.replace(/\$OG_DESCRIPTION/g, "Your Number one link box");
-      result = data.replace(/\$OG_IMAGE/g, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd1rf3y8sXzpn9HnJUpbn8rxfdhS1f2tHvPw&usqp=CAU');
+      const parsed = (request.url)
+      var url = require('url');
+
+      var q = url.parse(parsed, true);
+
+      var qdata = q.query
+
+      const {info, name, on_image} = qdata
+
+    
+      
+      
+ 
+// replace the special strings with server generated strings
+      data = data.replace(/\$OG_TITLE/g,name);
+      data = data.replace(/\$OG_DESCRIPTION/g,info);
+      result = data.replace(/\$OG_IMAGE/g,on_image);
       response.send(result);
     });
+
   });
 
 
+
+
+
+
+// axios.post(`http://localhost:8000/`)
+
+
+
+
+
+
+
+// app.get(`/music/${}}/id`, function(request, response) {
+ 
+//     const filePath = path.resolve(__dirname, './client/build' ,'index.html');
+  
+//     // read in the index.html file
+//     fs.readFile(filePath, 'utf8', function (err,data) {
+//       if (err) {
+//         return console.log(err);
+//       }
+      
+//       // replace the special strings with server generated strings
+//       data = data.replace(/\$OG_TITLE/g, 'Check Your Music ');
+//       data = data.replace(/\$OG_DESCRIPTION/g, "bigest platform NextPlatform HoME Enterterment Music Box");
+//       result = data.replace(/\$OG_IMAGE/g, 'http://nest-platform.herokuapp.com/static/media/Rosam-Im-free.96157f170e836c264ab6.jpeg');
+//       response.send(result);
+//     });
+//   });
 
 
 
