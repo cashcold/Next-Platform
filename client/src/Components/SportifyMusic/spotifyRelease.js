990834ee5f6373df4f531ed93b729b613b64360b
Frank99 <cashcold99@gmail.com>
2023-06-21 15:07:19 +0000

    update spotify new release received data pass

--- client/src/Components/SportifyMusic/spotifyRelease.js
+++ client/src/Components/SportifyMusic/spotifyRelease.js
@@ -3,21 +3,20 @@ import { Helmet } from 'react-helmet';
 import SpotifyWebApi from 'spotify-web-api-node';
 import SpotifyPlayer from 'react-spotify-web-playback';
 import axios from 'axios';
-import './spotifyDisplayMusic.css';
+import './spotifyRelease.css'; // Make sure to add the correct path to your CSS file
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
-import {Card,Button} from 'react-bootstrap'
-
-
+import { Card, Button } from 'react-bootstrap';
 
 class SpotifyReleases extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        Release_id: '',
-        accessToken: ''
-    
+      releaseId: '',
+      accessToken: '',
+      releaseData: null,
+      error: null,
     };
     this.spotifyApi = new SpotifyWebApi({
       clientId: '4e2ccdd89a0847bc992b541f5e5e6f73',
@@ -25,68 +24,66 @@ class SpotifyReleases extends Component {
   }
 
   componentDidMount() {
-    const Release_id = new URLSearchParams(window.location.search).get('Release_id');
-    // const Song_title = new URLSearchParams(window.location.search).get('Song_title');
-    // const Song_overview = new URLSearchParams(window.location.search).get('Song_overview');
-    // const Song_img = new URLSearchParams(window.location.search).get('Song_img');
+    const releaseId = new URLSearchParams(window.location.search).get('Release_id');
     const accessToken = new URLSearchParams(window.location.search).get('accessToken');
 
-    this.setState({Release_id, accessToken})
-   
+    this.setState({ releaseId, accessToken });
 
-
-
-    
-    setTimeout(()=>{
-      toast.dark(
-          <div className='logoImg animate__animated animate__slower animate__heartBeat welcome_trans_h4'>
-             
-              <Card >
-                  
-                  <Card.Body>
-                      <Card.Text>
-                      <div className="btc_shark_img">
-                          <a target='_blank' href='tel:+233203808479'>
-                          <img className="d-block w-100"  src={require('../../AllInOne/BTC_SHARK/A2 STICKER-01 (1).jpg')}
-                              alt="First slide" />
-                          </a>
-                          
-                          </div>
-                      </Card.Text>
-                  </Card.Body>
-                  </Card>
-          </div>, {
-          position: "top-right",
-          autoClose: false,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          });
-     },20000)
+    this.fetchReleaseData(releaseId, accessToken);
   }
 
+  fetchReleaseData(releaseId, accessToken) {
+    this.spotifyApi.setAccessToken(accessToken);
+
+    this.spotifyApi
+      .getAlbum(releaseId)
+      .then((response) => {
+        const releaseData = response.body;
+        this.setState({ releaseData });
+      })
+      .catch((error) => {
+        console.error('Error fetching release data:', error);
+        this.setState({ error });
+      });
+  }
 
   render() {
-  
+    const { releaseData, error } = this.state;
 
     return (
-      <div className="spotify-release-music">
-      
-        <ToastContainer/>
+      <div className="spotify-release-music ">
+        <ToastContainer />
         <h1 className="title">Next-Platform new_releases_section</h1>
-        
-         <section className="btc_shark_section">
-                <div className="btc_shark">
-                    <a target='_blank' href='tel:+233203808479'>
-                    <img className="d-block w-100"  src={require('../../AllInOne/BTC_SHARK/A2 STICKER-01 (1).jpg')}
-                        alt="First slide" />
-                    </a>
-                    
-                    </div>
+        <section className='release_info'>
+          {error ? (
+            <div className="error-message">Error fetching release data: {error.message}</div>
+          ) : releaseData ? (
+            <div className="release-data">
+              <div className="release-cover">
+                <img src={releaseData.images[0].url} alt="Release Cover" />
+              </div>
+              <div className="release-details">
+                <h2>{releaseData.name}</h2>
+                <p>Artist: {releaseData.artists[0].name}</p>
+                <p>Popularity: {releaseData.popularity}</p>
+                <p>Copyrights: {releaseData.label}</p>
+                <p>Release date: {releaseData.release_date}</p>
+                <p>Available Markets: {releaseData.available_markets.join(', ')}</p>
+              </div>
+              
+              <div className="spotify-player">
+                <SpotifyPlayer
+                  token={this.state.accessToken}
+                  uris={[releaseData.uri]}
+                  autoPlay={false}
+                  play={false}
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="loading-message">Loading release data...</div>
+          )}
         </section>
-
       </div>
     );
   }
