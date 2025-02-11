9f0a313304305f369271d1ffabaee476bc2319ef
Frank99 <cashcold99@gmail.com>
2025-02-11 05:24:50 -0800

    working update

--- client/src/Components/WithdrawRefferReward/WithdrawRefferReward.js
+++ client/src/Components/WithdrawRefferReward/WithdrawRefferReward.js
@@ -1,115 +1,105 @@
 import React, { Component } from 'react';
-import jwt_decode from 'jwt-decode';
-import './WithdrawRefferReward.css';
+import './WithdrawRefferReward.css'
 import axios from 'axios';
-import { toast } from 'react-toastify';
-import { ToastContainer } from 'react-toastify';
+import jwt_decode from 'jwt-decode';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import { io } from 'socket.io-client';
 
 class WithdrawRefferReward extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user_profile_display: '',
-      depositAmount: '0',
-      zero_accountBalance: '0',
       user_id: '',
-      user_Name: '',
-      type: 'Withdrawal',
-      email: '',
-      accountBalance: '',
-      bitcoin: '',
-      activetDeposit: '',
-      walletAddress: '',
-      withdraw_date: '',
-      activetDeposit__amount: '',
+      user_profile_display: {},
+      totalReferralReward: 0,
+      total: 0,
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.WithdrawNowFound = this.WithdrawNowFound.bind(this);
   }
-
-  handleChange = (input) => (event) => {
-    event.preventDefault();
-    this.setState({ [input]: event.target.value });
-  };
-
-  WithdrawNowFound = () => {
-    const userId = this.state.user_id;
-
-    // let socket = io('http://localhost:8000');
-    // socket.emit('WithdrawRefferReward', { userId });
-
-    axios.post(`http://localhost:8000/users/refferReward/${userId}`)
-      .then((res) => {
-        toast.success(res.data.message);
-      })
-      .then(() => {
-        setTimeout(() => {
-          window.location = '/dashboard';
-        }, 5000);
-      })
-      .catch((err) => {
-        toast.error("Failed to withdraw reward: " + (err.response?.data?.message || err.message));
-      });
-  };
 
   componentDidMount() {
     const token = sessionStorage.getItem('x-access-token');
-    const decoded = jwt_decode(token);
-    this.setState({
-      user_id: decoded.user_id,
-    });
+    if (token) {
+      const decoded = jwt_decode(token);
+      this.setState({ user_id: decoded.user_id });
 
-    const id = decoded.user_id;
+      // Fetch user profile
+      axios.post('/users/user_profile_display', { id: decoded.user_id })
+        .then(response => {
+          this.setState({ user_profile_display: response.data });
+        })
+        .catch(error => {
+          console.error("Error fetching user profile:", error);
+          toast.error("Failed to fetch user profile.");
+        });
 
-    axios.post('/users/user_profile_display', { id }).then((data) => this.setState({
-      user_profile_display: data.data,
-    }));
+      // Fetch total referral reward
+      axios.get(`/users/totalRefferReward/${decoded.user_id}`)
+        .then(response => {
+          this.setState({ totalReferralReward: response.data.totalReward });
+        })
+        .catch(error => {
+          console.error("Error fetching total referral reward:", error);
+          toast.error("Failed to fetch total referral reward.");
+        });
+    } else {
+      toast.error("No access token found.");
+    }
   }
 
+  handleWithdraw = () => {
+    const { user_id } = this.state;
+
+    axios.post('http://localhost:8000/users/withdrawReferralReward', { userId: user_id })
+      .then(response => {
+        toast.success(response.data.message);
+
+        // Update UI after successful withdrawal
+        this.setState(prevState => ({
+          user_profile_display: { ...prevState.user_profile_display, refferReward: 0 },
+          totalReferralReward: prevState.totalReferralReward - prevState.user_profile_display.refferReward,
+        }));
+      })
+      .then(res => setTimeout(()=>{
+        window.location='/withdraw-refferReward'
+      },100))
+
+      .catch(error => {
+        toast.error(error.response?.data?.message || "Withdrawal failed!");
+      });
+  };
+
   render() {
+    const { user_profile_display, totalReferralReward } = this.state;
+
     return (
       <div className='refferReward_center_main'>
         <div className='RefferReward__main'>
           <ToastContainer />
-          <h1 className='RefferReward__h1'>Reffer Reward Cashout</h1>
+          <h1 className='RefferReward__h1'>Referral Reward Cashout</h1>
 
           <div className="All__flow__withdraw">
             <section className='RefferReward__method_box'>
               <div className="flow__text">
                 <div className="with__inner__box_1">
-                  <h4>Reffer Reward:</h4>
-                </div>
-                <div className="with__inner__box_1">
-                  <h4>${this.state.user_profile_display.refferReward}.00</h4>
+                  <h4>Referral Reward:</h4>
+                  <h4>${user_profile_display.refferReward || '0'}.00</h4>
                 </div>
 
                 <div className="with__inner__box_1">
-                  <h4>Pending Reffer Reward:</h4>
-                </div>
-                <div className="with__inner__box_1">
-                  <h4>$0.00</h4>
+                  <h4>Total Referral Rewards:</h4>
+                  <h4>${totalReferralReward || '0'}.00</h4>
                 </div>
               </div>
-            </section>
 
-            <section className='RefferReward__wallet'>
-              <div className="iconWallet">
-                <i className="fas fa-wallet fa-3x"></i>
-              </div>
-              <div className="wallet__id">
-                <h4>Wallet Address</h4>
-              </div>
-              <div className="wallet__id">
-                <h4>{this.state.user_profile_display.bitcoin}</h4>
-              </div>
-              <section className='cash_reffer_reward_btn'>
-                <a href='#' className='btn btn-success btn-hover-effect' onClick={this.WithdrawNowFound}>
-                  WITHDRAW REWARD
-                </a>
-              </section>
+              {/* Withdraw Button */}
+              <button 
+                className="withdraw-button"
+                onClick={this.handleWithdraw}
+                disabled={user_profile_display.refferReward === 0}
+              >
+                Withdraw Reward
+              </button>
+
             </section>
           </div>
         </div>
